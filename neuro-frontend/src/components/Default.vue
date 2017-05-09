eda5ecc15114efb0e0f83a8b9bcd1315419afceb
hugohonda <hugohondaf@gmail.com>
2017-05-09 11:20:24 -0300

    create raw frontend  with mdl

---
+++ neuro-frontend/src/components/Default.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div(v-mdl="").custom-item-list
+    slot(name="item", v-for="item in value", :value="item")
+</template>
+<script>
+  import Card from './Card'
+  export default {
+    name: 'Default',
+    props: {
+      value: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    components: {
+      Card
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.custom-item-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
